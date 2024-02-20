8fb52b65bad84ee1a4f48e56230a17c5fa4bf3a8
Ivan Posazhennikov <ivanposazhennikov99@gmail.com>
2024-02-20 20:19:24 +0300

    🎉 init project

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /path/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "got path\n")
+	})
+
+	mux.HandleFunc("/note/{id}/", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "showing note with id=%v\n", id)
+	})
+
+	http.ListenAndServe("localhost:8090", mux)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vorokami/notes
+
+go 1.22.0
